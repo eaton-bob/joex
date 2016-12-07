97e63cc71c47db21638b276c9f87ccc2a90fca5b
Jana Rapava <janarapava@eaton.com>
2016-12-07 13:41:59 +0100

    Problem: We don't handle PING/PONG requests.
    Solution: Add the states.

--- include/joex_proto.h
+++ include/joex_proto.h
@@ -16,8 +16,7 @@ Set the data field, transferring ownership from caller
 #ifndef JOEX_PROTO_H_INCLUDED
 #define JOEX_PROTO_H_INCLUDED
 
-#include <joex.h>
-
+#include "joex.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
