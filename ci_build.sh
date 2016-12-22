7f940c85f0e57d9c8a34c7da3aacc80669e77148
Jim Klimov <jim@jimklimov.com>
2016-12-22 17:27:04 +0100

    ci_build.sh : excise ccache from PATH (and later CCACHE_PATH) if present

--- ci_build.sh
+++ ci_build.sh
@@ -19,9 +19,10 @@ if [ "$BUILD_TYPE" == "default" ] || [ "$BUILD_TYPE" == "default-Werror" ] ; the
     mkdir -p tmp .ccache
     BUILD_PREFIX=$PWD/tmp
 
+    PATH="`echo "$PATH" | sed -e 's,^/usr/lib/ccache/?:,,' -e 's,:/usr/lib/ccache/?:,,' -e 's,:/usr/lib/ccache/?$,,' -e 's,^/usr/lib/ccache/?$,,'2`"
     CCACHE_PATH="$PATH"
     CCACHE_DIR="$PWD/.ccache"
-    export CCACHE_PATH CCACHE_DIR
+    export CCACHE_PATH CCACHE_DIR PATH
     HAVE_CCACHE=no
     if which ccache && ls -la /usr/lib/ccache ; then
         HAVE_CCACHE=yes
